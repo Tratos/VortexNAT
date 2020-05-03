9d7856adaddcbab5f88556622b5551c9eb7b5868
Eisbaer's Public Git <35405475+Tratos@users.noreply.github.com>
2020-05-03 12:41:17 +0200

    Init
    Init

---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Open.Nat;
+using System.Threading;
+
+namespace Vortex_Test_IP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Helper.box = rtb1;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            string HostName = System.Net.Dns.GetHostName();
+            IPHostEntry host = Dns.GetHostEntry(HostName);
+            string IpAdresse = host.AddressList[0].ToString();
+
+            Helper.Log(HostName);
+
+            foreach (IPAddress address in host.AddressList)
+            {
+                Helper.Log($"    {address}");
+            }
+
+            Helper.Log("--------------");
+
+
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+
+            TestIP();
+
+
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            TestMapping();
+        }
+
+
+        private async void TestIP()
+        {
+            try
+            {
+                var discoverer = new NatDiscoverer();
+                var cts = new CancellationTokenSource(10000);
+                var device = await discoverer.DiscoverDeviceAsync(PortMapper.Upnp, cts);
+                var externTest = await device.GetExternalIPAsync();
+                string foo = externTest.ToString();
+
+                Helper.Log(foo);
+                Helper.Log("--------------");
+            }
+            catch (Exception Ex)
+            {
+                Helper.Log("Error: " + Ex);
+            }
+
+
+        }
+
+        private async void TestMapping()
+        {
+           try
+            {
+
+                var discoverer = new NatDiscoverer();
+                var device = await discoverer.DiscoverDeviceAsync();
+                await device.CreatePortMapAsync(new Mapping(Protocol.Tcp, 1601, 1701, "Open.Nat (Session lifetime)"));
+
+                var mappings = await device.GetAllMappingsAsync();
+
+                foreach (var mapping in mappings)
+                {
+                    Helper.Log($" {mapping}");
+                }
+
+                await device.DeletePortMapAsync(new Mapping(Protocol.Tcp, 1601, 1701));
+
+
+            }
+             catch (Exception Ex)
+            {
+                Helper.Log("Error: " + Ex);
+            }
+
+        }
+
+
+    }
+}
---
+++ Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Vortex_Test_IP
+{
+    public static class Helper
+    {
+        public static RichTextBox box = null;
+
+
+        public static void Log(string s)
+        {
+            if (box == null) return;
+            try
+            {
+                box.Invoke(new Action(delegate
+                {
+                    string stamp = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString() + " : ";
+                    box.AppendText(stamp + s + "\n");
+                    box.SelectionStart = box.Text.Length;
+                    box.ScrollToCaret();
+                }));
+            }
+            catch { }
+        }
+
+        public static void LogError(string who, Exception e, string cName = "")
+        {
+            string result = "";
+            if (who != "") result = "[" + who + "] " + cName + " ERROR: ";
+            result += e.Message;
+            if (e.InnerException != null)
+                result += " - " + e.InnerException.Message;
+            Log(result);
+        }
+
+    }
+}
